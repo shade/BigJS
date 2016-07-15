25119d1eff2151dacef5abc996cca57a9e87fce3
Joseph Thomas <joey6978@gmail.com>
2016-07-15 13:31:47 -0700

    Laying out new plans

---
+++ bigint.js
@@ -0,0 +1,19 @@
+/*
+* TYPES OF OPERATIONS WANTED
+*
+* -- ARITHMETIC
+*   - ADD
+*   - SUBTRACT
+*   - MULTIPLY
+*   - DIVIDE
+*   - MODULUS
+*   - POW
+*   - SQUARE
+*   - MODPOW
+*
+* -- BITWISE
+*   - NOT
+*   - OR
+*   - XOR
+*   - AND
+*/
