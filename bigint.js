28d914bbc10caedf49e550e37c2b3780818ee8d0
Joseph Thomas <joey6978@gmail.com>
2016-07-15 15:13:26 -0700

    Reiniting constructor

--- bigint.js
+++ bigint.js
@@ -24,9 +24,25 @@
 */
 
 
+// Each chunk in the big integer is 15 bits long. 
+var CHUNK_SIZE = 15;
+// Mask is like 000111 if there is a 3 bit chunk size
+var AND_MASK = (Math.pow(2,CHUNK_SIZE) - 1);
+// The inverse is just the opposite of the above mask
+var INV_AND_MASK = AND_MASK << CHUNK_SIZE;
 
 
+function BI(num,base){
+  if(num instanceof BI){
+    // Copy the big int
+  }
+  switch(typeof num){
+    case 'string':
 
-function BI(){
+    break;
+    case 'number':
+
+    break;
+  }
 
 }
