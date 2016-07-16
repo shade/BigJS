7435dbc0faeca3e73f080f2bcf03848b5873499b
Joseph Thomas <joey6978@gmail.com>
2016-07-15 17:08:08 -0700

    Need to finish right shifting

--- bigint-bitwise.js
+++ bigint-bitwise.js
@@ -5,7 +5,7 @@
 *
 */
 
-BI.prototype.lshift = function(x,me){
+BI.prototype.lshift = function(x, me){
   
   // If me is set, do the operation on the current Big Integer, else make a new one
   var _bi = me ? this : new BigInt(this);
@@ -47,6 +47,34 @@ BI.prototype.lshift = function(x,me){
     carry = _bi.n[i] >> CHUNK_SIZE;
 
   }
+ 
+}
+
+
+/*
+*  BI.rshift <- The function used to bit shift right the big integer
+*    @param x {Integer} - the number of places to shift 
+*    @param me {Boolean} - whether or not the shift should happen to the current object or make a new one.
+*
+*/
+
+BI.prototype.rshift = function(x, me){
+
+  // If me is set, do the operation on the current Big Integer, else make a new one
+  var _bi = me ? this : new BigInt(this);
+
+  // How many chunks and how many shifts left
+  var num = ~~(x / CHUNK_SIZE);
+  var rem = x % CHUNK_SIZE;
+
+  // Push out as many chunks as possible
+  for(var i = num;i--;){
+    _bi.n.shift();
+  }
+
+  //Resize
+  _bi.size -= num;
 
   
+
 }
