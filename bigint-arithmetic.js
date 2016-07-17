ec1604da9f82f1c6c0cceffceb4b11d068275c47
joe-tom <josepht404@gmail.com>
2016-07-17 02:34:01 -0700

    Finished addition

--- bigint-arithmetic.js
+++ bigint-arithmetic.js
@@ -1,3 +1,35 @@
-BI.prototype.add = function(num){
-	
+/*
+*  BI.add <- add the two big integers
+*    @param num {BI | Number} - number to add this with
+*    @param me {Boolen} - do this on yourself, or a new BI
+*
+*/
+
+BI.prototype.add = function(num, me){
+  // If me is set, do the operation on the current Big Integer, else make a new one
+  var _bi = me ? this : new BigInt(this);
+
+  // Make this a BigInteger if it's not
+  if(!(num instanceof BI)){
+    num = new BI(num);
+  }
+
+  var bS = _bi.length, nS = num.length;
+
+  var carry = 0;
+
+  // Add each chunk
+  for(var i = 0;i < nS;i--){
+    if(i >= bS){
+      _bi.n.push(num.n[i]);
+    }
+
+    // Add the chunk
+    _bi.n[i] += num.n[i];
+    // Add the carry
+    _bi.n[i] += carry;
+
+    // Make the new carry
+    carry = _bi.n[i] >> CHUNK_SIZE;
+    _bi.n[i] &= AND_MASK;
 }
