f01b47bebd1ec47c71cf73055693d7ce7a07c400
Joseph <joseph@vanH-MacBook-Pro.local>
2016-07-22 16:55:54 -0700

    Finished with the arithmatic operations

--- myBig.js
+++ myBig.js
@@ -4,7 +4,57 @@ function BI(){
 	return this;
 }
 
-This is the
-BI.prototype.add = function(num){
+/*
+*	BI.add - this function adds the current big int with another
+*		@param num {Number | BI} - the number you want to add to
+*		@param me {Boolean} - set to true if you want the addition to happen on the current object, else make a new obj
+*/
+BI.prototype.add = function(num, me){
+	var _this = (me ? this : new BI(this));
 
+	if(num instanceof BI){
+		_this.num = add(_this.num, num.num);
+	}else{
+		_this.num = addInt(_this.num, num);
+	}
+
+	return _this;
+}
+
+
+/*
+* BI.subtract - this function subtracts the current big int with another
+* 	@param num {Number | BI} - the number you want to subtract from
+*		@oaram me {Boolean}
+*/
+BI.prototype.subtract = function(num, me){
+	var _this = (me ? this : new BI(this));
+
+	//Subtraction is the same as adding the negative
+	if(num instanceof BI){
+		_this.num = sub(_this.num, num.num);
+	}else{
+		_this.num = addInt(_this.num, -num);
+	}
+
+	return _this;
+}
+
+
+/*
+* BI.multiply - this function multiplies the current big int with another
+* 	@param num {Number | BI} - the number you want to multiply with
+*		@oaram me {Boolean}
+*/
+BI.prototype.multiply = function(num, ne){
+	var _this = (me ? this : new BI(this));
+
+	if(num instanceof BI){
+		_this.num = mult(_this.num, num.num);
+	}else{
+		//this function automatically modifies the array
+		multInt_(_this.num,num);
+	}
+
+	return _this;
 }
