3a2f935a2eee739c60bec3d15180607480bd3692
Joseph <joseph@vanH-MacBook-Pro.local>
2016-07-22 19:20:09 -0700

    Added the toString function and updated the tests

--- test/construction.js
+++ test/construction.js
@@ -1,9 +1,18 @@
 describe('Construction And Digesting', function(){
-	describe("Decimal",function(){
-		it("Should have the same value as the first one",function(){
-			var number = ~~(Math.random() * 100000);
-			var big = new BI(number);
-			((big) * 1).should.equal(number);
+	describe("Digesting", function(){
+		describe("valueOf",function(){
+			it("Should have the same value as the first one",function(){
+				var number = ~~(Math.random() * 100000);
+				var big = new BI(number);
+				((big) * 1).should.equal(number);
+			});
 		});
-	})
-})
+		describe("toString",function(){
+			it("Should have the same value as the native version", function(){
+					var number = ~~(Math.random() * 100000);
+					var big = new BI(number);
+					big.toString(16).toUpperCase().should.equal(number.toString(16).toUpperCase());
+			});
+		});
+	});
+});
