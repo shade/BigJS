eaeb7c9ff0046363630af625e3efc175335c4ef7
joe-tom <josepht404@gmail.com>
2016-07-22 19:10:59 -0700

    Updated test and added constructor and valueof

--- test/construction.js
+++ test/construction.js
@@ -1,26 +1,9 @@
 describe('Construction And Digesting', function(){
-	describe('Decimal', function(){
-		describe('One Chunk', function(){
-
-			var num = ~~(Math.random());
-			it('should be the same as the first chunk', function(){
-				var a = new BI(num);
-				num.should.equal(a.n[0]);
-			});
-			it('should digest to the same', function(){
-				var a = new BI(num);
-				parseInt(a.digest(),2).should.equal(num);
-			})
-
+	describe("Decimal",function(){
+		it("Should have the same value as the first one",function(){
+			var number = ~~(Math.random() * 100000);
+			var big = new BI(number);
+			((big) * 1).should.equal(number);
 		});
-		describe('Multiple Chunks', function(){
-			//This is a big number
-			var num = Math.pow(2,~~(Math.random()*5)+16) - 1;
-			it('should digest to the same', function(){
-				var a = new BI(num);
-				console.log(a,num);
-				parseInt(a.digest(),2).should.equal(num);
-			})
-		})
 	})
 })
