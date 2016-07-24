ef4381f45f1cf66674cdde962683bd99856368e8
Joseph Thomas <joey6978@gmail.com>
2016-07-24 00:59:56 -0700

    Added modular exponentiation, but last test doesn't work

--- src/myBig--arithmetic.js
+++ src/myBig--arithmetic.js
@@ -1,7 +1,7 @@
 /*
 * BI.mod - this performs a modulus on the current big int
 *   @param num {BI | Number}
-*   @param me
+*   @param me {Boolean}
 */
 BI.prototype.mod = function(num, me){
   var _this = (me ? this : new BI(this));
@@ -12,5 +12,25 @@ BI.prototype.mod = function(num, me){
     _this.num = int2bigInt(modInt(_this.num,num),64);
   }
 
+  return _this;
+}
+
+
+/*
+* BI.pow - this performs an exponentiation using the current number as a base
+* this^num % mod
+*   @param num {BI | Number}
+*   @param mod {BI | Number}
+*   @param me {Boolean}
+*/
+BI.prototype.modpow = function(num, mod, me){
+  var _this = (me ? this : new BI(this));
+
+  //There are no integer functions, so this must be converted
+  num = (num instanceof BI ? num : new BI(num));
+  mod = (mod instanceof BI ? mod : new BI(mod));
+
+  _this.num = powMod(_this.num, num.num, mod.num);
+  
   return _this;
 }
